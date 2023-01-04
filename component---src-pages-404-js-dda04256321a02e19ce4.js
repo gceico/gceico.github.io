87f2ef4d79bd25fc44794346a7b47ca88d0ebe86
Gabriel Ceicoschi <ceicoschi.gabriel@gmail.com>
2023-01-04 11:54:07 +0200

    Updates

--- component---src-pages-404-js-dda04256321a02e19ce4.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),o=n("wBNj");t.default=function(e){e.location;return l.a.createElement(o.Container,null,l.a.createElement(o.Row,{justify:"center"},l.a.createElement(o.Col,{lg:6},l.a.createElement("h1",null,"404 NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist..."))))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-dda04256321a02e19ce4.js.map
